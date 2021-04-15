ec9a785558ca95da4590186141640beaa0c10dec
ooolin321 <69572105+ooolin321@users.noreply.github.com>
2021-04-15 13:39:09 +0800

    live_order + live_report model

--- CEA103G4/src/com/live_order/model/Live_orderDAO_interface.java
+++ CEA103G4/src/com/live_order/model/Live_orderDAO_interface.java
@@ -3,9 +3,13 @@ package com.live_order.model;
 import java.util.*;
 
 public interface Live_orderDAO_interface {
-    public void insert(Live_orderVO live_orderVO);
-    public void update(Live_orderVO live_orderVO);
-    public void delete(Integer live_order_no);
-    public Live_orderVO findByPrimaryKey(Integer live_order_no);
-    public List<Live_orderVO> getAll();
+	public void insert(Live_orderVO live_orderVO);
+
+	public void update(Live_orderVO live_orderVO);
+
+	public void delete(Integer live_order_no);
+
+	public Live_orderVO findByPrimaryKey(Integer live_order_no);
+
+	public List<Live_orderVO> getAll();
 }
